180072d39b2b5e1e39c602fddd0e26d64a3f63a3
rafacrbll <rafacrbll.info@gmail.com>
2025-05-21 09:54:20 +0200

    Arreglo de Header

--- resources/views/componentes/header.blade.php
+++ resources/views/componentes/header.blade.php
@@ -6,19 +6,16 @@
         
         <div class="sb-pages">
             <div class="nav">
-                <a class="nav-link" href="{{ url('/cartas/buscar') }}" title="Buscar" style="margin-top: 40px;">
-                    <div class="sb-nav-link-icon"><i class="bi bi-search" style="color: #606060"></i></div>
+                <a class="nav-link" href="{{ url('/cartas/buscar') }}" title="Subir Carta" style="margin-top: 40px;">
+                    <div class="sb-nav-link-icon"><i class="bi bi-file-earmark-arrow-up-fill" style="color: #606060"></i></div>
                 </a>
                 <a class="nav-link" href="{{ route('inicio') }}" title="Inicio" style="margin-top: 40px;">
                     <div><i class="bi bi-house-fill" style="color: #606060"></i></div>
                 </a>
-                <a class="nav-link" href="#" title="Chat" style="margin-top: 40px;">
-                    <div><i class="bi bi-envelope-fill" style="color: #606060"></i></div>
-                </a>
                 <a class="nav-link" href="{{ route('catalogo') }}" title="Catálogo" style="margin-top: 40px;">
                     <div><i class="bi bi-images" style="color: #606060"></i></div>
                 </a>
-                <a class="nav-link" href="{{ route('cartas.mis') }}" title="Configuración" style="margin-top: 40px;">
+                <a class="nav-link" href="{{ route('cartas.mis') }}" title="Mis Cartas" style="margin-top: 40px;">
                     <div><i class="bi bi-gear-fill" style="color: #606060"></i></div>
                 </a>
                 @auth
